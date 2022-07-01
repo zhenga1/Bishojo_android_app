43e64165a3beb4a6dd1371bead211722eeef3505
Aaron Zheng <aaronhaowenzheng@gmail.com>
2022-07-01 15:48:30 +0800

    completed splash screen

---
+++ app/src/main/java/com/zhenga1/bisojo/SplashActivity.java
@@ -0,0 +1,31 @@
+package com.zhenga1.bisojo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.WindowManager;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        ///status bar hide starting //////
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        ///status bar hide end///////
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashActivity.this, MainActivity.class);
+                startActivity(intent);
+                SplashActivity.this.finish();
+                //Finish activity
+            }
+        },3000);
+
+    }
+}
